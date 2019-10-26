b12fff95de9d1733ba2ee69ec362a816cf386165
simiyen <onurkantr@gmail.com>
2019-10-26 13:18:39 +0300

    Evaluation Changed

---
+++ CMSLibrary/Answers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMSLibrary
+{
+    public class Answers
+    {
+        public string Type { get; set; }
+        public string True { get; set; }
+        public int Number { get; set; }
+
+    }
+}
---
+++ CMSLibrary/info.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMSLibrary
+{
+    public class info
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string No { get; set; }
+        public string Group { get; set; }
+        public string Answers { get; set; }
+        public int CorrectNumber { get; set; }
+    }
+}
